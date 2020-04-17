cd86b7d005db75149635ac9d8e3cf43c7348b2b0
Hicham <tony.theedev@gmail.com>
2020-04-18 00:21:42 +0100

    adding SQl script file

---
+++ MyDatabase.sql
@@ -0,0 +1,10 @@
+create database test
+create table PersoneTest
+(
+    id_personne int primary key,
+    nom_personne varchar(20),
+    date_naissance date,
+    sexe varchar(10)
+);
+
+--select * from PersoneTest
